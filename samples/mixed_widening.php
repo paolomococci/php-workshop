c88a082869dcbef07695d4bb8de66e80e955b1b8
paolomococci <paolomococci@gmail.com>
2023-03-28 21:59:20 +0200

    modified:   samples/mixed_widening.php

--- samples/mixed_widening.php
+++ samples/mixed_widening.php
@@ -3,11 +3,37 @@
 declare(strict_types=1);
 
 class SomeOne {
-    const LOG_FILE = __DIR__ . '/../test/someone.log';
+    const LOG_FILE = __DIR__ . '/../test/tests.log';
     protected static function logVar(object $v) {
         $item = date('Y-m-d') . ':' . var_export($v, TRUE);
         return error_log($item, 3, self::LOG_FILE);
     }
 }
 
-class SomeTwo {}
+class SomeTwo extends SomeOne {
+    public static function logVar(mixed $v) {
+        $item = date('Y-m-d') . ':' . var_export($v, TRUE);
+        return error_log($item, 3, self::LOG_FILE);
+    }
+}
+
+if(file_exists(SomeOne::LOG_FILE)) unlink(SomeOne::LOG_FILE);
+
+$tests = [
+    'array' => range('B', 'V'),
+    'task' => function () {
+        return __CLASS__;
+    },
+    'unknown' => new class () {
+        public function __invoke() 
+        {
+            return __CLASS__;
+        }
+    }
+];
+
+foreach ($tests as $test) {
+    SomeTwo::logVar($test);
+}
+
+readfile(SomeOne::LOG_FILE);
