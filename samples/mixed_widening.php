2a46e8305ef370924628dab3cb6d339ffafcf8a1
paolomococci <paolomococci@gmail.com>
2023-03-28 21:27:46 +0200

    modified:   samples/mixed_widening.php

--- samples/mixed_widening.php
+++ samples/mixed_widening.php
@@ -2,6 +2,12 @@
 
 declare(strict_types=1);
 
-class SomeOne {}
+class SomeOne {
+    const LOG_FILE = __DIR__ . '/../test/someone.log';
+    protected static function logVar(object $v) {
+        $item = date('Y-m-d') . ':' . var_export($v, TRUE);
+        return error_log($item, 3, self::LOG_FILE);
+    }
+}
 
 class SomeTwo {}
