a705cd60c3293fb5df54a3bf3f67492a40b33111
paolomococci <paolomococci@gmail.com>
2023-03-28 22:17:50 +0200

    modified:   samples/typed_property.php

--- samples/typed_property.php
+++ samples/typed_property.php
@@ -1,3 +1,18 @@
 <?php
 
 declare(strict_types=1);
+
+use SomeOne as GlobalSomeOne;
+
+class Someone {
+    public int $id = 0;
+    public int $code = 0;
+    public string $name = '';
+}
+
+$someone = new Someone();
+
+$someone->id = 111;
+$someone->code = 222;
+//$someone->name = 1;
+$someone->name = 'qwerty';
