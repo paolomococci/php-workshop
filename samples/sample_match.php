ade0b4282241c80af8cebad349f63c3606feb240
paolomococci <paolomococci@gmail.com>
2023-03-29 17:18:25 +0200

    modified:   samples/sample_match.php

--- samples/sample_match.php
+++ samples/sample_match.php
@@ -1,35 +1,36 @@
 <?php
 
-function getNumber(string $stringNumber): string {
-  return match ($stringNumber) {
-    'zero' => '0',
-    'one' => '1',
-    'two' => '2',
-    'three' => '3',
-    'four' => '4',
-    'five' => '5',
-    'six' => '6',
-    'seven' => '7',
-    'eight' => '8',
-    'nine' => '9',
-  };
+function getNumber(string $stringNumber): string
+{
+    return match($stringNumber) {
+        'zero' => '0',
+        'one' => '1',
+        'two' => '2',
+        'three' => '3',
+        'four' => '4',
+        'five' => '5',
+        'six' => '6',
+        'seven' => '7',
+        'eight' => '8',
+        'nine' => '9',
+    };
 }
 
 $samples = [
-  'zero',
-  'one',
-  'two',
-  'three',
-  'four',
-  'five',
-  'six',
-  'seven',
-  'eight',
-  'nine'
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
 ];
 
 foreach ($samples as $sample) {
-  echo 'the word ' . $sample . ' corresponds to ' . getNumber($sample) . "\n";
+    echo 'the word ' . $sample . ' corresponds to ' . getNumber($sample) . "\n";
 }
 
 echo "\n";
