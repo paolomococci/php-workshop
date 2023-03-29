e3ab3c78dbbefd2fb2e8f052df26df711c4b0345
paolomococci <paolomococci@gmail.com>
2023-03-29 09:12:18 +0200

    modified:   samples/nullsafe_operator.php

--- samples/nullsafe_operator.php
+++ samples/nullsafe_operator.php
@@ -3,3 +3,14 @@
 declare(strict_types=1);
 
 $xml = simplexml_load_file(__DIR__ . '/extra/assortment.xml');
+
+$products = [
+  'product' => [
+    'some1',
+    'some2',
+    'some3',
+    'some4',
+    'some5',
+    'some6',
+  ]
+];
