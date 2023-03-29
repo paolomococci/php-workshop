39db088cdf6eac316d2a99064f991edd02d52dc9
paolomococci <paolomococci@gmail.com>
2023-03-29 18:38:45 +0200

    modified:   samples/serialize_json.php

--- samples/serialize_json.php
+++ samples/serialize_json.php
@@ -3,7 +3,16 @@
 declare (strict_types = 1);
 
 class Response
-{}
+{
+    public function formalize($data) {
+        return json_encode(
+            $data,
+            JSON_PRETTY_PRINT
+        );
+    }
+}
 
 class Serialize
-{}
+{
+    public function formalize() {}
+}
