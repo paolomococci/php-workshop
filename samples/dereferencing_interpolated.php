bc6173d5efc193137498ee4353add6bacc13e081
paolomococci <paolomococci@gmail.com>
2023-03-29 11:15:07 +0200

    modified:   samples/dereferencing_interpolated.php

--- samples/dereferencing_interpolated.php
+++ samples/dereferencing_interpolated.php
@@ -1,3 +1,25 @@
 <?php
 
 declare(strict_types=1);
+
+$alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+$numbers = '0123456789';
+$dot = '.';
+$dash = '-';
+
+$items = [
+  15,
+  7,
+  15,
+  37,
+  34,
+  36,
+  28,
+  36,
+  30
+];
+
+foreach ($items as $item) 
+  echo "$alpha$numbers$dot$dash"[$item];
+
+  echo "\n";
