a9ea85795dfb995bdd511b3e4fbf2b11558ae91b
paolomococci <paolomococci@gmail.com>
2023-03-29 17:18:07 +0200

    modified:   samples/mixed_widening.php

--- samples/mixed_widening.php
+++ samples/mixed_widening.php
@@ -1,35 +1,42 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types = 1);
 
-class SomeOne {
+class SomeOne
+{
     const LOG_FILE = __DIR__ . '/tests/test_one.log';
-    protected static function logVar(object $argument) {
-        $item = date('Y-m-d') . ':' . var_export($argument, TRUE);
+    protected static function logVar(object $argument)
+    {
+        $item = date('Y-m-d') . ':' . var_export($argument, true);
         return error_log($item, 3, self::LOG_FILE);
     }
 }
 
-class SomeTwo extends SomeOne {
-    public static function logVar(mixed $argument) {
-        $item = date('Y-m-d') . ':' . var_export($argument, TRUE);
+class SomeTwo extends SomeOne
+{
+    public static function logVar(mixed $argument)
+    {
+        $item = date('Y-m-d') . ':' . var_export($argument, true);
         return error_log($item, 3, self::LOG_FILE);
     }
 }
 
-if(file_exists(SomeOne::LOG_FILE)) unlink(SomeOne::LOG_FILE);
+if (file_exists(SomeOne::LOG_FILE)) {
+    unlink(SomeOne::LOG_FILE);
+}
 
 $tests = [
     'array' => range('B', 'V'),
     'task' => function () {
         return __CLASS__;
     },
-    'unknown' => new class () {
-        public function __invoke() 
-        {
+    'unknown' => new class()
+{
+        public function __invoke()
+    {
             return __CLASS__;
         }
-    }
+    },
 ];
 
 foreach ($tests as $test) {
