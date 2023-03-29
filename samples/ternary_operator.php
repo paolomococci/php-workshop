5e3c225d8129b432840d41fdd30c3b59baf75ff5
paolomococci <paolomococci@gmail.com>
2023-03-29 09:53:29 +0200

    modified:   samples/ternary_operator.php

--- samples/ternary_operator.php
+++ samples/ternary_operator.php
@@ -1,3 +1,15 @@
 <?php
 
 declare(strict_types=1);
+
+class SomeOne {
+  public function someone(
+    int $id
+  ): string {
+      return ($id > 0) ? "positive\n" : "negative\n";
+  }
+}
+
+$someOne = new SomeOne();
+
+echo $someOne->someone(-5);
