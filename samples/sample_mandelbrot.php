1bde43268066743d4ab9e4ab42b0195dc3ba90a3
paolomococci <paolomococci@gmail.com>
2023-04-02 07:53:39 +0200

    modified:   samples/sample_mandelbrot.php

--- samples/sample_mandelbrot.php
+++ samples/sample_mandelbrot.php
@@ -5,3 +5,10 @@ declare (strict_types = 1);
 define('RELIVE', 16);
 define('LOOPS', 16384);
 define('EDGE', 40.0);
+
+$start_time = microtime(true);
+
+function replay(
+    $abscissa,
+    $ordinate
+) {}
