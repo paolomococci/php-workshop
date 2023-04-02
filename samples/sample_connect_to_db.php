1e00625eea7cdece413b554e6aba835f26bc68a2
paolomococci <paolomococci@gmail.com>
2023-04-02 23:57:04 +0200

    modified:   samples/sample_connect_to_db.php

--- samples/sample_connect_to_db.php
+++ samples/sample_connect_to_db.php
@@ -5,3 +5,9 @@ declare (strict_types = 1);
 $server_name = "127.0.0.1";
 $user_name = "username";
 $password = "password";
+
+try {
+    // TODO
+} catch (PDOException $e) {
+    echo "database connection failed: " . $e->getMessage();
+}
