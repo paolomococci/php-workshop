2b0975574229e14ce40b55d92f0121162b2ccc03
paolomococci <paolomococci@gmail.com>
2023-04-03 00:02:46 +0200

    modified:   samples/sample_connect_to_db.php

--- samples/sample_connect_to_db.php
+++ samples/sample_connect_to_db.php
@@ -3,11 +3,16 @@
 declare (strict_types = 1);
 
 $server_name = "127.0.0.1";
+$db_name = "sample";
 $user_name = "username";
 $password = "password";
 
 try {
-    // TODO
+    $conn = new PDO(
+        "mysql:host=$server_name;dbname=$db_name", 
+        $user_name, 
+        $password
+    );
 } catch (PDOException $e) {
-    echo "database connection failed: " . $e->getMessage();
+    echo "connection to database $db_name failed with the following message: " . $e->getMessage();
 }
