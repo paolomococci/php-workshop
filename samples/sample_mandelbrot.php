c13746aee43c00ba821871ca2c177d81cc09716c
paolomococci <paolomococci@gmail.com>
2023-04-02 09:36:18 +0200

    modified:   samples/sample_mandelbrot.php

--- samples/sample_mandelbrot.php
+++ samples/sample_mandelbrot.php
@@ -17,8 +17,23 @@ function replay(
     $z_r = 0.0;
     $z_i = 0.0;
     $index = 0;
-    
-    return 0;
+
+    while (true) {
+        $index++;
+        $temp = $z_r * $z_i;
+        $z_r_square = $z_r * $z_r;
+        $z_i_square = $z_i * $z_i;
+        $z_r = $z_r_square - $z_i_square + $c_r;
+        $z_i = $temp + $temp + $c_i;
+        
+        if ($z_i_square + $z_r_square > RELIVE) {
+            return $index;
+        }
+
+        if ($index > LOOPS) {
+            return 0;
+        }
+    }
 }
 
 function mandelbrot(
