8b02f0bd554336a086c60d958649f26bc07d720b
paolomococci <paolomococci@gmail.com>
2023-04-03 22:04:16 +0200

    modified:   notebooks/_0_read_values_from_a_text_file.php

--- notebooks/_0_read_values_from_a_text_file.php
+++ notebooks/_0_read_values_from_a_text_file.php
@@ -5,7 +5,10 @@ declare (strict_types = 1);
 $values = file_get_contents("../.env");
 
 $lines = explode("\n", $values);
+$items = array();
 
 foreach ($lines as $line) {
-    echo "$line\n";
+    $items[] = substr($line, strpos($line, '='));
 }
+
+print_r($items);
