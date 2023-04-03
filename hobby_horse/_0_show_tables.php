e82dd55c2469076b7bd9b86714bca36e0f877aae
paolomococci <paolomococci@gmail.com>
2023-04-03 22:04:45 +0200

    modified:   hobby_horse/_0_show_tables.php

--- hobby_horse/_0_show_tables.php
+++ hobby_horse/_0_show_tables.php
@@ -1,3 +1,12 @@
 <?php
 
 declare (strict_types = 1);
+
+$values = file_get_contents("../.env");
+
+$lines = explode("\n", $values);
+$items = array();
+
+foreach ($lines as $line) {
+    $items[] = substr($line, strpos($line, '='));
+}
