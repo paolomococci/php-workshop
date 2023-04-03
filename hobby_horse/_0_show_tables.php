31348bad7a8d459dd65f59ddcfb10fd5603ff989
paolomococci <paolomococci@gmail.com>
2023-04-03 22:17:23 +0200

    modified:   hobby_horse/_0_show_tables.php

--- hobby_horse/_0_show_tables.php
+++ hobby_horse/_0_show_tables.php
@@ -8,7 +8,7 @@ $lines = explode("\n", $values);
 $items = array();
 
 foreach ($lines as $line) {
-    $items[] = substr($line, strpos($line, '='));
+    $items[] = substr($line, strpos($line, '=')+1);
 }
 
 $rdbms = $items[0];
@@ -22,3 +22,28 @@ $db_password = $items[6];
 $show_tables_query = <<< 'EOD'
 SHOW TABLES;
 EOD;
+
+try {
+
+    $conn = new PDO(
+        "$rdbms:host=$db_host;port=$db_port;charset=$db_charset;dbname=$db_database",
+        $db_username,
+        $db_password
+    );
+
+    $conn->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+    echo "connection to database $db_database was successful\n";
+
+    foreach ($conn->query($show_tables_query) as $row) {
+        print_r($row);
+    }
+
+    $conn = null;
+
+} catch (PDOException $e) {
+    echo "connection to database $db_database failed with the following message: " . $e->getMessage() . "\n";
+    die();
+}
